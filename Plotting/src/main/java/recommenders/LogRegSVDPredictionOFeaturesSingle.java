851e6655d9c0778be459096178cd63022298b60c
steffi <sd.deckers@student.maastrichtuniversity.nl>
2017-01-25 00:45:20 +0100

    added plots, added appendix, added results for new prediction rules plot

---
+++ Plotting/src/main/java/recommenders/LogRegSVDPredictionOFeaturesSingle.java
@@ -0,0 +1,149 @@
+package recommenders;
+
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Polygon;
+import java.awt.RadialGradientPaint;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import javax.swing.JPanel;
+
+import de.erichseifert.gral.data.DataSeries;
+import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.graphics.Label;
+import de.erichseifert.gral.graphics.Location;
+import de.erichseifert.gral.plots.XYPlot;
+import de.erichseifert.gral.plots.axes.AxisRenderer;
+import de.erichseifert.gral.plots.axes.LogarithmicRenderer2D;
+import de.erichseifert.gral.plots.legends.Legend;
+import de.erichseifert.gral.plots.legends.SeriesLegend;
+import de.erichseifert.gral.plots.legends.ValueLegend;
+import de.erichseifert.gral.plots.lines.DefaultLineRenderer2D;
+import de.erichseifert.gral.plots.lines.DiscreteLineRenderer2D;
+import de.erichseifert.gral.plots.lines.LineRenderer;
+import de.erichseifert.gral.plots.points.DefaultPointRenderer2D;
+import de.erichseifert.gral.plots.points.PointRenderer;
+import de.erichseifert.gral.plots.points.SizeablePointRenderer;
+import de.erichseifert.gral.ui.InteractivePanel;
+import de.erichseifert.gral.util.GraphicsUtils;
+import de.erichseifert.gral.graphics.Insets2D;
+import de.erichseifert.gral.graphics.Orientation;
+
+public class LogRegSVDPredictionOFeaturesSingle extends ExamplePanel{
+	public static void main(String[] args) {
+		String file_name = "..\\SVDPredictionOFeatures.csv";
+		new LogRegSVDPredictionOFeaturesSingle(file_name);
+	}
+	
+	private static final Random random = new Random();
+	
+	/** First corporate color used for normal coloring.*/
+	protected static final Color COLOR1 = new Color( 55, 170, 200);
+	/** Second corporate color used as signal color */
+	protected static final Color COLOR2 = new Color(200,  80,  75);
+	
+	protected static final Color COLOR3 = Color.GREEN;
+	protected static final Color COLOR4 = Color.YELLOW;
+	protected static final Color COLOR5 = Color.CYAN;
+	protected static final Color COLOR6 = Color.MAGENTA;
+
+	
+	public LogRegSVDPredictionOFeaturesSingle(String file_name) {
+		Path path = FileSystems.getDefault().getPath(file_name);
+
+		DataTable dataTable = null;
+		// Read data from file into dataTable
+		try {
+			BufferedReader reader = Files.newBufferedReader(path);
+			String line = reader.readLine();
+			String[] headers = line.split(",");
+			int fieldsNum = headers.length;
+			dataTable = new DataTable(fieldsNum, Double.class);
+			
+			while((line = reader.readLine()) != null){
+				String[] fields = line.split(",");
+				Double[] dataFields = Arrays.stream(fields).map(x -> Double.parseDouble(x)).toArray(size -> new Double[size]);
+				dataTable.add(dataFields);
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Plot
+		DataSeries series1 = new DataSeries("No weighting", dataTable, 0, 5);
+		DataSeries series2 = new DataSeries("Article-based weighting", dataTable, 0, 6);
+		XYPlot plot = new XYPlot(series1, series2);
+		plot.setLegendVisible(true);
+		plot.getLegend().setAlignmentX(1);
+		plot.getLegend().setAlignmentY(1);
+		plot.getPlotArea().setBorderStroke(null);
+		
+		// Format plot
+		plot.setInsets(new Insets2D.Double(70.0, 70.0, 70.0, 70.0));
+		plot.setBackground(Color.WHITE);
+		plot.getTitle().setText(getDescription());
+		
+		// Format axes
+		AxisRenderer axisRendererX = plot.getAxisRenderer(XYPlot.AXIS_X);
+		AxisRenderer axisRendererY = plot.getAxisRenderer(XYPlot.AXIS_Y);
+		axisRendererY.setLabelDistance(2);
+//		plot.getAxis(XYPlot.AXIS_Y).setRange(0.06, 0.12);
+		axisRendererX.setLabel(new Label("# features"));
+		axisRendererY.setLabel(new Label("MAP"));
+		plot.setAxisRenderer(XYPlot.AXIS_X, axisRendererX);
+		plot.setAxisRenderer(XYPlot.AXIS_Y, axisRendererY);
+		
+		// Format rendering of data points
+		PointRenderer pointRenderer1 = new DefaultPointRenderer2D();
+		pointRenderer1.setColor(GraphicsUtils.deriveDarker(COLOR1));
+		pointRenderer1.setShape(new Rectangle2D.Double(-3.5,-3.5,7,7));
+		plot.setPointRenderers(series1, pointRenderer1);
+		
+		PointRenderer pointRenderer2 = new DefaultPointRenderer2D();
+		pointRenderer2.setColor(GraphicsUtils.deriveDarker(COLOR2));
+		pointRenderer2.setShape(new Rectangle2D.Double(-3.5,-3.5,7,7));
+		plot.setPointRenderers(series2, pointRenderer2);
+		
+		
+		// Format data lines
+		LineRenderer lineRenderer1 = new DefaultLineRenderer2D();
+		lineRenderer1.setColor(COLOR1);
+		plot.setLineRenderers(series1, lineRenderer1);
+		
+		LineRenderer lineRenderer2 = new DefaultLineRenderer2D();
+		lineRenderer2.setColor(COLOR2);
+		plot.setLineRenderers(series2, lineRenderer2);
+		
+		// Add plot to Swing component
+		add(new InteractivePanel(plot), BorderLayout.CENTER);
+		showInFrame();
+	}
+
+	@Override
+	public String getTitle() {
+		return "LogRegSVD";
+	}
+
+	@Override
+	public String getDescription() {
+		return "LogRegSVD";
+}
+}
